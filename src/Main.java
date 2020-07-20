61ecebcb7a4cb924075f3bf60471b8a0bb3b5b4e
Kristina Sachenko <mytino4ka@mail.ru>
2020-07-20 23:39:58 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean registered = true;
+        int quantity = 20;
+        int price = 2000;
+        long miles = price / quantity;
+        long limit = 500;
+
+        System.out.println(miles);
+
+    }
+}
