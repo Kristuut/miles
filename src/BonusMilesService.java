00cfe45c097f290664173e12a62d989541b35750
Kristina Sachenko <mytino4ka@mail.ru>
2020-07-30 00:42:00 +0700

    Добавлен класс calculate

---
+++ src/BonusMilesService.java
@@ -0,0 +1,13 @@
+public class BonusMilesService {
+    public long calculate (int cost) {
+        int quantity = 20;
+        int price = 10_000;
+        long miles = price / quantity;
+
+        return miles;
+
+    }
+
+    }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int quantity = 20;
-        int price = 2000;
-        long miles = price / quantity;
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        long miles = service.calculate(price);
 
         System.out.println(miles);
-
     }
 }
+
+
+
+
